3dcab962d1c3f9d54ee29fb82885ee0ed2f80620
kenta7777 <k.hasegw7@gmail.com>
2022-03-02 20:57:47 +0900

    rewrite App.tsx as function component

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import github_image from './github-mark.png';
 import './App.css';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-              <header className="App-header">
-                <h1> Kenta7777's Portfolio </h1>
-        
-                <Header2 name="About Me"/>
-                <p>I'm Kenta7777, newbie software engineer.</p>
-                <p> <a href="https://github.com/kenta7777"> <img src={github_image} alt="GitHub"></img> </a> </p>
-        
-                <Header2 name="Main Interests"/>
-                <ul> 
-                    <MainInterestsItem name="Rust"/>
-                    <MainInterestsItem name="Type System"/>
-                    <MainInterestsItem name="Compiler"/>
-                </ul>
-              </header>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+            <h1> Kenta7777's Portfolio </h1>
+    
+            <Header2 name="About Me"/>
+            <p>I'm Kenta7777, newbie software engineer.</p>
+            <p> <a href="https://github.com/kenta7777"> <img src={github_image} alt="GitHub"></img> </a> </p>
+    
+            <Header2 name="Main Interests"/>
+            <ul> 
+                <MainInterestsItem name="Rust"/>
+                <MainInterestsItem name="Type System"/>
+                <MainInterestsItem name="Compiler"/>
+            </ul>
+            </header>
+        </div>       
+    );
 }
 
 type Props = {
