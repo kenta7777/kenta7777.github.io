8608ccbd8f057f17611f24e456a25c23634425f7
kenta7777 <k.hasegw7@gmail.com>
2023-04-20 12:33:31 +0900

    route setting for Home and Blog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Blog from './Pages/Blog';
 
 const App = () => {
     return (
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Blog" element={<Blog />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import MainInterests from "../../Components/MainInterests";
 import github_image from '../../github-mark.png';
 import './index.css';
@@ -13,7 +14,7 @@ const Home = () => {
         <p>I'm Kenta7777, newbie software engineer.</p>
         <div className="link-box">
             <p> <a rel="noreferrer" target="_blank" href="https://github.com/kenta7777"> <img src={github_image} alt="GitHub"></img> </a> </p>
-            <p> <a rel="noreferrer" target="_blank" href="https://www.google.com/?hl=ja"> Blog </a> </p>
+            <p> <Link to="/Blog">Blog</Link> </p>
         </div>
         <h2>Main Interests</h2>
         <MainInterests names={MainInterestsNames} />
