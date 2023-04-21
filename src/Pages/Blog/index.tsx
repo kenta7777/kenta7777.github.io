74f717495d9784c5a65d9b1df33f0943b813812e
kenta7777 <k.hasegw7@gmail.com>
2023-04-21 15:44:34 +0900

    split blog content into component

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -1,27 +1,12 @@
-import markdownit from 'markdown-it';
-import { useEffect, useState } from 'react';
 import './index.css';
-import samplePath from '../../BlogContents/Sample.md';
+import samplePath from '../../BlogResources/Sample.md';
+import BlogContent from '../../Components/BlogContent';
 
-const Blog = () => {
-    const [blogContent, setBlogContent] = useState('');
-    useEffect(() => {
-        fetch(samplePath).then(res => res.text()).then(text => setBlogContent(text))
-    });
-
-    const md = markdownit();
-    const emoji = require('markdown-it-emoji');
-    const prism = require('markdown-it-prism');
-    const mark = require('markdown-it-mark');
-    const footnote = require('markdown-it-footnote');
-    md.use(emoji).use(prism).use(mark).use(footnote);
-
-    return (
-        <div className="page">
-            <div className="page-title"> Blog </div>
-            <div dangerouslySetInnerHTML={{__html: md.render(blogContent)}}/>
-        </div>
-    )
-};
+//TODO: read all blog resources from local
+const Blog = () => 
+    <div className="page">
+        <div className="page-title"> Blog </div>
+        <BlogContent resourcePath={samplePath} />
+    </div>
 
 export default Blog;
